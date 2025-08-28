da6b02b12af43df6aab8e8a92389e761b632e6c7
Henry-cmd325 <henrycanalesv@gmail.com>
2025-08-27 21:47:35 -0600

    fix: Error en SongEditUserControl corregido

--- NitroSongs.Desktop/Modules/Songs/UserControls/SongEditUserControl.xaml.cs
+++ NitroSongs.Desktop/Modules/Songs/UserControls/SongEditUserControl.xaml.cs
@@ -34,9 +34,51 @@ public sealed partial class SongEditUserControl : UserControl
     public string Lyrics
     {
         get => (string)GetValue(LyricsProperty);
-        set => SetValue(LyricsProperty, value);
+        set 
+        {
+            SetValue(LyricsProperty, value);
+            ViewModel.LoadTokens(value);
+        }
     }
 
     public static readonly DependencyProperty LyricsProperty =
-        DependencyProperty.Register(nameof(Lyrics), typeof(string), typeof(SongViewerControl), new PropertyMetadata(""));
+        DependencyProperty.Register(nameof(Lyrics), typeof(string), typeof(SongEditUserControl), new PropertyMetadata(""));
+
+    // Al iniciar el drag desde un chip de acorde
+    private void Chord_DragStarting(UIElement sender, DragStartingEventArgs args)
+    {
+        //if (sender is Border b && b.DataContext is string chord)
+        //{
+        //    args.Data.SetText(chord);
+        //    args.Data.RequestedOperation = Windows.ApplicationModel.DataTransfer.DataPackageOperation.Copy;
+
+        //    // Visual opcional:
+        //    args.DragUI.SetContentFromDataPackage();
+        //}
+    }
+
+    // Mientras arrastras sobre un token (para permitir el drop)
+    private void Token_DragOver(object sender, DragEventArgs e)
+    {
+        //if (e.DataView.Contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.Text))
+        //{
+        //    e.AcceptedOperation = Windows.ApplicationModel.DataTransfer.DataPackageOperation.Copy;
+        //}
+        //e.Handled = true;
+    }
+
+    // Sueltas el acorde sobre un token
+    private async void Token_Drop(object sender, DragEventArgs e)
+    {
+        //if (sender is FrameworkElement fe && fe.DataContext is LyricToken token)
+        //{
+        //    if (e.DataView.Contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.Text))
+        //    {
+        //        var chord = await e.DataView.GetTextAsync();
+        //        token.Chord = chord?.Trim();
+        //        token.HasChord = !string.IsNullOrEmpty(token.Chord);
+        //    }
+        //}
+        //e.Handled = true;
+    }
 }
--- NitroSongs.Desktop/Modules/Songs/ViewModels/SongEditViewModel.cs
+++ NitroSongs.Desktop/Modules/Songs/ViewModels/SongEditViewModel.cs
@@ -1,13 +1,42 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.UI.Xaml;
+using NitroSongs.Services.Songs;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NitroSongs.Modules.Songs.ViewModels
 {
+    public class LyricToken : DependencyObject
+    {
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(LyricToken), new PropertyMetadata(""));
+
+        public string Chord
+        {
+            get => (string)GetValue(ChordProperty);
+            set => SetValue(ChordProperty, value);
+        }
+        public static readonly DependencyProperty ChordProperty =
+            DependencyProperty.Register(nameof(Chord), typeof(string), typeof(LyricToken), new PropertyMetadata(""));
+
+        public bool HasChord
+        {
+            get => (bool)GetValue(HasChordProperty);
+            set => SetValue(HasChordProperty, value);
+        }
+        public static readonly DependencyProperty HasChordProperty =
+            DependencyProperty.Register(nameof(HasChord), typeof(bool), typeof(LyricToken), new PropertyMetadata(false));
+    }
     public partial class SongEditViewModel : ObservableObject
     {
         [ObservableProperty]
@@ -16,12 +45,31 @@ namespace NitroSongs.Modules.Songs.ViewModels
         [ObservableProperty]
         private bool isVisualMode = true;
 
-        public bool IsTextMode => !IsVisualMode;
+        [ObservableProperty]
+        private bool isTextMode = false;
+
+        public ObservableCollection<LyricToken> LyricTokens { get; } = [];
+
+        [RelayCommand]
+        public void LoadTokens(string Lyrics)
+        {
+            LyricTokens.Clear();
+            LyricTokens.Add(new LyricToken { Text = "h", Chord = "", HasChord = false });
+            LyricTokens.Add(new LyricToken { Text = "o", Chord = "", HasChord = false });
+            LyricTokens.Add(new LyricToken { Text = "l", Chord = "", HasChord = false });
+            LyricTokens.Add(new LyricToken { Text = "a", Chord = "", HasChord = false });
+            LyricTokens.Add(new LyricToken { Text = "e", Chord = "D", HasChord = true });
+            LyricTokens.Add(new LyricToken { Text = "s", Chord = "C", HasChord = true });
+            LyricTokens.Add(new LyricToken { Text = "u", Chord = "", HasChord = false });
+            LyricTokens.Add(new LyricToken { Text = "n", Chord = "", HasChord = false });
+            LyricTokens.Add(new LyricToken { Text = "a", Chord = "", HasChord = false });
+        }
 
         [RelayCommand]
         public void SwitchMode() 
         {
             IsVisualMode = !IsVisualMode;
+            IsTextMode = !IsTextMode;
 
             if (IsVisualMode)
                 BtnText = "Modo Texto";
